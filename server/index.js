2ddee88fa92b3233d33046d6cc666af09d9995da
John David Gaspar <johndavidgaspar1107@gmail.com>
2025-04-20 09:05:43 +0800

    Added logger file

--- server/index.js
+++ server/index.js
@@ -11,17 +11,21 @@ import CreateFeedback from './routers/user/create-feedback.js';
 import ForgotPassword from './routers/forgotpassword.js';
 import ResetPassword from './routers/resetpassword.js';
 import ProductRouter from './routers/admin/product.js';
+import logger from './middleware/logger.js';
 
 
 dotenv.config();
-
 const app = express();
+
+app.use(logger);
 
 app.use(express.json());
 app.use(cookieParser());
 
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({origin: 'http://localhost:5173' , credentials: true}));
+app.use(cors({origin: 'http://localhost:5173', 
+credentials: true,
+methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH']}));
 
 app.use(signUpRouter, signInRouter, AdminRouter,
 AdminLoginRouter, CreateFeedback, ForgotPassword, 
@@ -31,5 +35,7 @@ app.listen(process.env.PORT, () => {
     console.log(`Server is running on PORT: ${process.env.PORT}`);
     connectDB();
 });
+
+
 
 
