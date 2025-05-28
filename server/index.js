254bd2d9c587269906a57de65947e819a8677edb
John David Gaspar <johndavidgaspar1107@gmail.com>
2025-05-29 04:46:41 +0800

    Fixed some bugs

--- server/index.js
+++ server/index.js
@@ -32,7 +32,8 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: process.env.FRONTEND_URL, 
-    methods: ['GET', 'POST']
+    methods: ['GET', 'POST'],
+    credentials: true
   }
 });
 
