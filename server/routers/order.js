2b5b831904fccb0e70f2984c40653650a1be4d73
John David Gaspar <johndavidgaspar1107@gmail.com>
2025-05-29 03:24:47 +0800

    Fixed some bugs

--- server/routers/order.js
+++ server/routers/order.js
@@ -60,6 +60,7 @@ OrderRouter.get('/orders', authToken, async (req, res) => {
 
 OrderRouter.put('/orders/:id', authToken, async (req, res) => {
   try {
+    const userId = req.user._id; 
     const order = await Order.findById(req.params.id);
     if (!order) {
       return res.status(404).json({ success: false, message: 'Order not found' });
