6c7ef81cc5f1993df48d540058a5dd48fc73d13c
John David Gaspar <johndavidgaspar1107@gmail.com>
2025-05-29 03:30:52 +0800

    Fixed some bugs

--- server/models/order.js
+++ server/models/order.js
@@ -4,7 +4,6 @@ const orderSchema = new mongoose.Schema({
   userId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
-    required: true
   },
   productId: {
     type: mongoose.Schema.Types.ObjectId,
