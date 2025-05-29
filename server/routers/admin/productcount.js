e405a505bdca6e844c74ae3b2d79afca525f2aac
John David Gaspar <johndavidgaspar1107@gmail.com>
2025-05-29 08:21:34 +0800

    Fixed some bugs

--- server/routers/admin/productcount.js
+++ server/routers/admin/productcount.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import CountProduct from '../../controllers/admin/countproduct.js';
+import authToken from '../../middleware/authToken.js';
 
 const ProductCountRouter = express.Router();
 
-ProductCountRouter.get('/count-product', CountProduct);
+ProductCountRouter.get('/count-product', authToken, CountProduct);
 
 
 export default ProductCountRouter;
--- server/routers/admin/usercount.js
+++ server/routers/admin/usercount.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import UserCount from '../../controllers/admin/countuser.js';
+import authToken from '../../middleware/authToken.js';
 
 const UserCountRouter = express.Router();
 
-UserCountRouter.get('/user-count', UserCount );
+UserCountRouter.get('/user-count', authToken, UserCount );
 
 export default UserCountRouter;
