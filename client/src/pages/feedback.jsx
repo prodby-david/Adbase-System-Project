2d9420b30133a7ed16cc437f238b8ac0560938b9
John David Gaspar <johndavidgaspar1107@gmail.com>
2025-05-29 17:22:57 +0800

    Fixed some bugs

--- client/src/pages/feedback.jsx
+++ client/src/pages/feedback.jsx
@@ -11,7 +11,7 @@ const Feedback = () => {
       useEffect(() => {
         const fetchFeedback = async () => {
             try {
-                const response = await axios.get(`${baseUrl}/api/user-feedback`);
+                const response = await axios.get(`${baseUrl}/user-feedback`);
                 setFeedback(response.data);
             } catch (error) {
                 console.error('Error fetching feedback:', error.message);
